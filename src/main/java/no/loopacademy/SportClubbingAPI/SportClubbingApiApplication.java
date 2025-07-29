2be003f27dc2d36b4747d1a712f48e3b50322792
Dr. Dean von Schoultz <deanvons@gmail.com>
2025-07-29 10:34:37 +0200

    initial commit

---
+++ src/main/java/no/loopacademy/SportClubbingAPI/SportClubbingApiApplication.java
@@ -0,0 +1,13 @@
+package no.loopacademy.SportClubbingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportClubbingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportClubbingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/loopacademy/SportClubbingAPI/SportClubbingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package no.loopacademy.SportClubbingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportClubbingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
